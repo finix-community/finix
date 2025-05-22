aaaa67d4e729d1fd39f95e534173cee993f16dcd
Aaron Andersen <aaron@fosslib.net>
2025-05-21 20:18:01 -0400

    modules/bluetooth: provide some minimal configuration

--- modules/services/bluetooth/default.nix
+++ modules/services/bluetooth/default.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 let
   cfg = config.services.bluetooth;
+
+  format = pkgs.formats.ini { };
 in
 {
   options.services.bluetooth = {
@@ -18,10 +20,26 @@ in
       type = lib.types.bool;
       default = false;
     };
+
+    settings = lib.mkOption {
+      type = format.type;
+      default = { };
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    services.bluetooth.settings = {
+      General = {
+        ControllerMode = lib.mkDefault "dual";
+      };
+
+      Policy = {
+        AutoEnable = lib.mkDefault true;
+      };
+    };
+
     environment.systemPackages = [ cfg.package ];
+    environment.etc."bluetooth/main.conf".source = format.generate "main.conf" cfg.settings;
 
     services.dbus.packages = [ cfg.package ];
     services.udev.packages = [ cfg.package ];
@@ -29,7 +47,7 @@ in
     finit.services.bluetooth = {
       description = "bluetooth service";
       conditions = [ "service/syslogd/ready" "service/dbus/ready" ];
-      command = "${cfg.package}/bin/bluetoothd" + lib.optionalString cfg.debug " -d";
+      command = "${cfg.package}/libexec/bluetooth/bluetoothd -f /etc/bluetooth/main.conf" + lib.optionalString cfg.debug " -d";
     };
   };
 }
