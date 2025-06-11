662f94567f6424162c81e06af17d2db778ecf06d
Emery Hemingway <emery@slow.janky.email>
2025-06-11 09:18:15 -0400

    modules/dhcpcd: synit service, conditional udev support

--- tests/synit.nix
+++ tests/synit.nix
@@ -4,12 +4,15 @@ testenv.mkTest {
   name = "synit";
   nodes.machine = {
     boot.serviceManager = "synit";
+    services.dhcpcd.enable = true;
   };
   tclScript = ''
     machine spawn
     machine expect {synit_pid1: Awaiting signals...}
     machine expect {syndicate_server: inferior server instance}
     machine expect {<milestone system-machine>*{state: up}}
+    machine expect {eth0: carrier acquired}
+    machine expect {eth0: adding default route via 10.0.2.2}
     success
   '';
 }
