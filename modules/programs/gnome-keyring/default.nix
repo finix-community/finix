752a844c09c8c06b3b2ea763e2a86f14d94c0305
Aaron Andersen <aaron@fosslib.net>
2025-06-14 08:33:06 -0400

    modules/gnome-keyring: new module

---
+++ modules/programs/gnome-keyring/default.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.programs.gnome-keyring;
+in
+{
+  options.programs.gnome-keyring.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.gnome-keyring ];
+
+    services.dbus.packages = [
+      pkgs.gnome-keyring
+      pkgs.gcr
+    ];
+
+    xdg.portal.portals = [
+      pkgs.gnome-keyring
+    ];
+
+    security.wrappers.gnome-keyring-daemon = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_ipc_lock=ep";
+      source = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon";
+    };
+  };
+}
