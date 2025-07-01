854f647f4885eace16c1c6883e931261883a0230
Emery Hemingway <emery@slow.janky.email>
2025-07-01 15:33:17 -0500

    modules/greetd: synit daemon

--- modules/services/greetd/default.nix
+++ modules/services/greetd/default.nix
@@ -13,15 +13,20 @@ in
     };
 
     settings = lib.mkOption {
+      description = ''
+        Contents of the greetd configuration file.
+        See the greetd(5) man page.
+      '';
       type = format.type;
-      default = { };
     };
   };
 
   config = lib.mkIf cfg.enable {
+
     services.greetd.settings = {
-      terminal.vt = 7;
+      terminal.vt = lib.mkDefault "next";
       default_session = {
+        command = lib.mkDefault "${pkgs.greetd.greetd}/bin/agreety";
         user = "greeter";
       };
     };
@@ -32,6 +37,14 @@ in
       conditions = [ "service/syslogd/ready" ] ++ lib.optionals config.services.seatd.enable [ "service/seatd/ready" ];
       command = "${pkgs.greetd.greetd}/bin/greetd --config ${configFile}";
       cgroup.name = "user";
+    };
+
+    synit.daemons.greetd = {
+      argv = [ "${pkgs.greetd.greetd}/bin/greetd" "--config" configFile ];
+      provides = [ [ "milestone" "login" ] ];
+      requires = [ { key = [ "milestone" "wrappers" ]; } ]
+        ++ lib.optional config.services.seatd.enable
+          { key = [ "daemon" "seatd" ]; state = "ready"; };
     };
 
     users.users = {
@@ -53,14 +66,13 @@ in
       "d /var/cache/tuigreet - greeter greeter"
     ];
 
-    # pulled from lemurs
-    security.pam.services.greetd = {
-      text = ''
+    security.pam = {
+      enable = true;
+      services.greetd.text = ''
         # Account management.
         account required pam_unix.so # unix (order 10900)
 
         # Authentication management.
-        auth optional pam_unix.so likeauth nullok # unix-early (order 11500)
         auth sufficient pam_unix.so likeauth nullok try_first_pass # unix (order 12800)
         auth required pam_deny.so # deny (order 13600)
 
@@ -68,16 +80,16 @@ in
         password sufficient pam_unix.so nullok yescrypt # unix (order 10200)
 
         # Session management.
-        session required pam_env.so readenv=1 debug # env (order 10100)
+        session required pam_env.so readenv=0 # env (order 10100)
         session required pam_unix.so # unix (order 10200)
-        # https://github.com/coastalwhite/lemurs/issues/166
-        # session optional pam_loginuid.so # loginuid (order 10300)
-
-        ${lib.optionalString config.services.elogind.enable "session optional ${pkgs.elogind}/lib/security/pam_elogind.so"}
-        ${lib.optionalString config.services.seatd.enable "session optional ${pkgs.pam_rundir}/lib/security/pam_rundir.so"}
-
-        session required ${pkgs.linux-pam}/lib/security/pam_lastlog.so silent # lastlog (order 10700)
-      '';
+        session required pam_loginuid.so # loginuid (order 10300)
+      ''
+      + lib.optionalString config.services.elogind.enable
+        "session optional ${pkgs.elogind}/lib/security/pam_elogind.so"
+      + lib.optionalString config.services.seatd.enable
+        "session optional ${pkgs.pam_rundir}/lib/security/pam_rundir.so"
+      ;
     };
+
   };
 }
