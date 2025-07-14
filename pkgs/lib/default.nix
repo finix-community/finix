9b025e4f63be76ed6b8aff99b14f58b80d9b40eb
Emery Hemingway <emery@slow.janky.email>
2025-07-14 13:16:49 +0000

    lib: add quoteExecline

--- pkgs/lib/default.nix
+++ pkgs/lib/default.nix
@@ -1,6 +1,15 @@
 lib: prev:
 
-{
+rec {
+  # Convert a command-line that uses lists for
+  # execline blocks into the quoted block format.
+  # This is what execlineb does before executing.
+  quoteExecline = builtins.foldl' (acc: arg: acc ++ (
+    if builtins.isList arg
+    then map (_: " ${_}") (quoteExecline arg) ++ [ "" ]
+    else [ arg ]
+  )) [ ];
+
   generators = prev.generators // {
     toPreserves = import ./generators/preserves.nix { inherit lib; };
   };
