3afb798cb677ed13146fa42cb39a22b57d160a51
Emery Hemingway <emery@slow.janky.email>
2025-07-22 17:17:46 +0100

    modules/fuse: move FUSE configuration to dedicated module

---
+++ modules/filesystems/fuse.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.boot.supportedFilesystems.fuse;
+  cfgInitrd = config.boot.initrd.supportedFilesystems.fuse;
+in
+{
+  options = let
+      fuseEnable = default: {
+        supportedFilesystems.fuse.enable = lib.mkOption {
+          type = lib.types.bool;
+          inherit default;
+        };
+      };
+    in {
+      boot = (fuseEnable true) // { initrd = fuseEnable false; };
+    };
+
+  config = lib.mkIf (cfg.enable || cfgInitrd.enable) {
+
+    boot = let
+        modsIf = v: lib.mkIf v [ "fuse" ];
+      in {
+        kernelModules = modsIf cfg.enable;
+        initrd.kernelModules = modsIf cfgInitrd.enable;
+      };
+
+    security.wrappers = let
+        mkSetuidRoot = source: {
+          setuid = true; owner = "root"; group = "root"; inherit source;
+        };
+      in {
+        fusermount = mkSetuidRoot "${pkgs.fuse}/bin/fusermount";
+        fusermount3 = mkSetuidRoot "${pkgs.fuse3}/bin/fusermount3";
+      };
+
+    services.mdevd.hotplugRules = ''
+      fuse 0:0 666
+    '';
+  };
+}
