0b2e7813d3ff26780711e474dd1046de31eed3ca
Emery Hemingway <emery@slow.janky.email>
2025-08-05 08:33:45 +0000

    modules/synit: persistent daemon services

--- modules/synit/default.nix
+++ modules/synit/default.nix
@@ -123,7 +123,12 @@ in
 
     system.activation.scripts.synit-config = {
       deps = [ "specialfs" ];
-      text = "install --mode=755 --directory /run/synit/config/{core,machine,network,profile,state}";
+      text = ''
+        for D in /etc/syndicate/core /run/synit/{,config/{,core,machine,network,persistent,profile,state}}; do
+          s6-mkdir -m 750 -p $D
+          s6-chown -g 1 $D
+        done
+      '';
     };
 
   };
