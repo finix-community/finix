46e529b562d84d608a58dd9ca356eeddb29be33c
Aaron Andersen <aaron@fosslib.net>
2025-10-12 21:29:11 -0400

    modules/system76-scheduler: init

---
+++ modules/services/system76-scheduler/default.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.services.system76-scheduler;
+in
+{
+  options.services.system76-scheduler = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.system76-scheduler;
+    };
+
+    debug = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    configFile = lib.mkOption {
+      type = lib.types.path;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.etc."system76-scheduler/config.kdl".source = cfg.configFile;
+    environment.systemPackages = [ cfg.package ];
+    services.dbus.packages = [ cfg.package ];
+
+    finit.services.system76-scheduler = {
+      description = "system76 scheduler";
+      command = "${lib.getExe cfg.package} daemon";
+      reload = "${lib.getExe cfg.package} daemon reload";
+      conditions = [ "service/syslogd/ready" "service/dbus/ready" ];
+      log = true;
+
+      # TODO: now we're hijacking `env` and no one else can use it...
+      env = pkgs.writeText "system76-scheduler.env" (''
+        NO_COLOR=1
+        PATH="${lib.makeBinPath [ pkgs.kmod pkgs.gnutar pkgs.xz ]}:$PATH"
+      '' + lib.optionalString cfg.debug ''
+        RUST_LOG=system76_scheduler=debug
+      '');
+    };
+  };
+}
