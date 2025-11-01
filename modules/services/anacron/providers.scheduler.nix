7f4e52847bbe9ca8118e1039b36deb13b085a5a8
Aaron Andersen <aaron@fosslib.net>
2025-10-31 21:13:14 -0400

    modules/anacron: init

---
+++ modules/services/anacron/providers.scheduler.nix
@@ -0,0 +1,35 @@
+{ config, lib, ... }:
+let
+  anacronCompatible = v: v.interval == "daily" || v.interval == "weekly" || v.interval == "monthly" || v.interval == "yearly";
+in
+{
+  options.providers.scheduler = {
+    backend = lib.mkOption {
+      type = lib.types.enum [ "anacron" ];
+    };
+  };
+
+  config = lib.mkIf (config.providers.scheduler.backend == "anacron") {
+    providers.scheduler.supportedFeatures = {
+      user = false;
+    };
+
+    services.cron.systab = lib.mapAttrsToList (k: v:
+      let
+        expr =
+          if v.interval == "hourly" then "0 0 * * *"
+          else v.interval
+        ;
+
+        user =
+          if v.user != null then v.user
+          else "root";
+      in
+        "${expr} ${user} ${v.command}"
+    ) (lib.filterAttrs (k: v: ! anacronCompatible v) config.providers.scheduler.tasks);
+
+    services.anacron.systab = lib.mapAttrsToList (k: v:
+      "@${v.interval} 0 ${k} ${v.command}"
+    ) (lib.filterAttrs (k: anacronCompatible) config.providers.scheduler.tasks);
+  };
+}
