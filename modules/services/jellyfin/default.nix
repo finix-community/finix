790c14d317a2e64a3ee2081e2f55af594acfc2b2
Aaron Andersen <aaron@fosslib.net>
2025-11-01 21:48:36 -0400

    modules/jellyfin: add dataDir, user, and group options

--- modules/services/jellyfin/default.nix
+++ modules/services/jellyfin/default.nix
@@ -20,32 +20,76 @@ in
         The package to use for `jellyfin`.
       '';
     };
+
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/jellyfin";
+      description = ''
+        The directory used to store all `jellyfin` data.
+
+        ::: {.note}
+        If left as the default value this directory will automatically be created on
+        system activation, otherwise you are responsible for ensuring the directory exists
+        with appropriate ownership and permissions before the `jellyfin` service starts.
+        :::
+      '';
+    };
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "jellyfin";
+      description = ''
+        User account under which `jellyfin` runs.
+
+        ::: {.note}
+        If left as the default value this user will automatically be created
+        on system activation, otherwise you are responsible for
+        ensuring the user exists before the `jellyfin` service starts.
+        :::
+      '';
+    };
+
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = "jellyfin";
+      description = ''
+        Group account under which `jellyfin` runs.
+
+        ::: {.note}
+        If left as the default value this group will automatically be created
+        on system activation, otherwise you are responsible for
+        ensuring the group exists before the `jellyfin` service starts.
+        :::
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     finit.services.jellyfin = {
+      inherit (cfg) user group;
+
       description = "jellyfin media server";
       conditions = "service/syslogd/ready";
-      command = "${lib.getExe cfg.package} --datadir /var/lib/jellyfin --configdir /var/lib/jellyfin/config --cachedir /var/cache/jellyfin --logdir /var/log/jellyfin";
-      user = "jellyfin";
-      group = "jellyfin";
+      command = "${lib.getExe cfg.package} --datadir ${cfg.dataDir} --configdir ${cfg.dataDir}/config --cachedir /var/cache/jellyfin --logdir /var/log/jellyfin";
+      nohup = true;
     };
 
     services.tmpfiles.jellyfin.rules = [
-      "d /var/cache/jellyfin 0700 jellyfin jellyfin"
-      "d /var/lib/jellyfin 0700 jellyfin jellyfin"
-      "d /var/lib/jellyfin/config 0700 jellyfin jellyfin"
-      "d /var/log/jellyfin 0750 jellyfin jellyfin"
+      "d /var/cache/jellyfin 0700 ${cfg.user} ${cfg.group}"
+      "d /var/log/jellyfin 0750 ${cfg.user} ${cfg.group}"
+    ] ++ lib.optionals (cfg.dataDir == "/var/lib/jellyfin") [
+      "d ${cfg.dataDir} 0700 ${cfg.user} ${cfg.group}"
+      "d ${cfg.dataDir}/config 0700 ${cfg.user} ${cfg.group}"
     ];
 
-    users.users = {
+    users.users = lib.optionalAttrs (cfg.user == "jellyfin") {
       jellyfin = {
         group = "jellyfin";
         isSystemUser = true;
       };
     };
 
-    users.groups = {
+    users.groups = lib.optionalAttrs (cfg.group == "jellyfin") {
       jellyfin = { };
     };
   };
