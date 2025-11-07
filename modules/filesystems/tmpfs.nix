96d2e1391c087d53c4c00723043b381715b5e855
Aaron Andersen <aaron@fosslib.net>
2025-11-07 16:06:44 -0500

    modules/filesystems: remove unused `packages` options

--- modules/filesystems/tmpfs.nix
+++ modules/filesystems/tmpfs.nix
@@ -6,22 +6,12 @@
         type = lib.types.bool;
         default = true;
       };
-
-      packages = lib.mkOption {
-        type = with lib.types; listOf package;
-        default = [ ];
-      };
     };
 
     boot.supportedFilesystems.tmpfs = {
       enable = lib.mkOption {
         type = lib.types.bool;
         default = true;
-      };
-
-      packages = lib.mkOption {
-        type = with lib.types; listOf package;
-        default = [ ];
       };
     };
   };
