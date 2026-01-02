82d18b4af05a875dd6adb1f9e8ac3d8c3c88bccc
Aaron Andersen <aaron@fosslib.net>
2026-01-01 21:17:54 -0500

    testing: temporarily tweak some tests

--- tests/finit.nix
+++ tests/finit.nix
@@ -5,12 +5,27 @@
 testenv.mkTest {
   name = "finit";
 
-  nodes.machine = {
-    boot.serviceManager = "finit";
+  nodes.machine =
+    { pkgs, ... }:
+    {
+      boot.serviceManager = "finit";
 
-    finit.runlevel = 2;
-    services.mdevd.enable = true;
-  };
+      finit.runlevel = 2;
+      services.mdevd.enable = true;
+
+      finit.package = pkgs.finit.overrideAttrs (finalAttrs: {
+        version = "4.15";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "aanderse";
+          repo = "finit";
+          rev = "373738f3d15a928ab8b37dcc2a3ac47df68db824";
+          sha256 = "sha256-O7UgEWX55xzIEii5RB0F0qWk4UAl1n/F6ERF3GO/axA=";
+        };
+
+        buildInputs = finalAttrs.buildInputs ++ [ pkgs.libcap ];
+      });
+    };
 
   testScript = ''
     machine start
