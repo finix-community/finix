7a843296306c3323aaa2f702c97a986382916499
Aaron Andersen <aaron@fosslib.net>
2026-01-15 20:43:02 -0500

    wip, fixup tests

--- tests/finix-test-driver/shell.nix
+++ tests/finix-test-driver/shell.nix
@@ -11,8 +11,15 @@
       finit.runlevel = 2;
       services.mdevd.enable = true;
 
-      finit.package = pkgs.finit.overrideAttrs (o: {
-        patches = o.patches or [ ] ++ [ ./netlink.patch ];
+      finit.package = pkgs.finit.overrideAttrs (finalAttrs: {
+        version = "4.15";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "aanderse";
+          repo = "finit";
+          rev = "29029bb78f513876665a64072e066db9a18d2241";
+          sha256 = "sha256-X9ORF/OkSD2nrMzPXT9p3+GYI0Fa/5KDRl5p42Z7maA=";
+        };
       });
     };
 
