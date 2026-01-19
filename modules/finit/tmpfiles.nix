ac29786ec9703065d196b6297114b66cf163c7f9
Aaron Andersen <aaron@fosslib.net>
2026-01-18 20:32:13 -0500

    modules/finit: run `tmpfiles --clean` daily

--- modules/finit/tmpfiles.nix
+++ modules/finit/tmpfiles.nix
@@ -15,8 +15,6 @@
   };
 
   config = {
-    # TODO: improve finit so tmpfiles can cleanup aged files
-
     environment.etc."tmpfiles.d/finix.conf".text = ''
       # This file is created automatically and should not be modified.
       # Please change the option ‘finit.tmpfiles.rules’ instead.
@@ -32,14 +30,14 @@
 
     finit.tasks.tmpfiles-setup.command = "${config.finit.package}/libexec/finit/tmpfiles --create";
 
-    # TODO: run once a day
-    # providers.scheduler.tasks = {
-    #   tmpfiles-clean = {
-    #     interval = "daily";
-    #     command = "${config.finit.package}/libexec/tmpfiles --clean";
-    #   };
-    # };
+    providers.scheduler.tasks = {
+      tmpfiles-clean = {
+        interval = "daily";
+        command = "${config.finit.package}/libexec/finit/tmpfiles --clean";
+      };
+    };
 
-    # needed for finit tmpfiles implementation: pkgs.policycoreutils
+    # needed for finit tmpfiles Z implementation: pkgs.policycoreutils
+    # TODO: make this an optional dependency, fixup Z behaviour in general
   };
 }
