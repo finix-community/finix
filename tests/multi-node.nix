408956d47c633e02e61b48cea92b4ad8a10dc8f8
Aaron Andersen <aaron@fosslib.net>
2026-04-21 16:38:32 +0900

    tests/multi-node: fixup: explicitly wait for network connections

--- tests/multi-node.nix
+++ tests/multi-node.nix
@@ -25,6 +25,10 @@
     client.wait_for_console_text("entering runlevel 2")
     server.wait_for_console_text("entering runlevel 2")
 
+    # wait for network connection
+    client.wait_until_succeeds(f"initctl cond get net/eth0/running")
+    server.wait_until_succeeds(f"initctl cond get net/eth0/running")
+
     with subtest("nodes have correct ips"):
         client_ip = client.succeed("ip addr show eth0")
         assert "192.168.1.1" in client_ip, f"client missing expected ip 192.168.1.1: {client_ip}"
