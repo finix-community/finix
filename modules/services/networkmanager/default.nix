18af2607cb4a3d74e867f733ddc19ec19314be54
Aaron Andersen <aaron@fosslib.net>
2026-07-09 17:38:14 -0400

    modules/networkmanager: add `settings` option

--- modules/services/networkmanager/default.nix
+++ modules/services/networkmanager/default.nix
@@ -7,6 +7,8 @@
 let
   cfg = config.services.networkmanager;
 
+  format = pkgs.formats.ini { };
+
   packages = [
     cfg.package
     pkgs.wpa_supplicant
@@ -30,25 +32,62 @@ in
         The package to use for `networkmanager`.
       '';
     };
+
+    settings = lib.mkOption {
+      type = lib.types.submodule {
+        freeformType = format.type;
+        options = {
+          main.rc-manager = lib.mkOption {
+            type = lib.types.enum [
+              "auto"
+              "file"
+              "netconfig"
+              "none"
+              "resolvconf"
+              "symlink"
+              "unmanaged"
+            ];
+            default = "symlink";
+            description = ''
+              Set the `resolv.conf` management mode.
+            '';
+          };
+        };
+      };
+      default = { };
+      description = ''
+        `networkmanager` configuration. See {manpage}`NetworkManager.conf(5)`
+        for additional details.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    services.networkmanager.settings.main = lib.optionalAttrs config.programs.resolvconf.enable {
+      # nixpkgs builds NetworkManager with a hardcoded resolvconf path, so the
+      # default rc-manager=auto always selects resolvconf, even when nothing
+      # has set it up, silently dropping DNS updates.
+      rc-manager = lib.mkDefault "resolvconf";
+    };
+
     boot.kernelModules = [
       "ctr"
     ];
 
     environment.systemPackages = packages;
+    environment.etc."NetworkManager/conf.d/00-nixos.conf".source =
+      format.generate "00-nixos.conf" cfg.settings;
+
+    # TODO: add finit.services.reloadTriggers option
+    environment.etc."finit.d/network-manager.conf".text = lib.mkAfter ''
+
+      # reload trigger
+      # ${config.environment.etc."NetworkManager/conf.d/00-nixos.conf".source}
+    '';
 
     services.dbus.enable = true;
     services.dbus.packages = packages;
     services.udev.packages = packages;
-
-    # nixpkgs builds NetworkManager with a hardcoded resolvconf path, so the
-    # default rc-manager=auto always selects resolvconf, even when nothing
-    # has set it up, silently dropping DNS updates.
-    environment.etc."NetworkManager/conf.d/00-nixos.conf".text = lib.generators.toINI { } {
-      main.rc-manager = if config.programs.resolvconf.enable then "resolvconf" else "symlink";
-    };
 
     finit.services.network-manager = {
       description = "network manager service";
