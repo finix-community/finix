b2d14a40a6a66d524bd0ddf8a7adb07db7e14fc9
willowispll <gorelikgleb+github@gmail.com>
2026-08-01 17:44:08 +0300

    fix(dinit): pass D-Bus startup script as a command string

--- modules/services/dbus/dbus-dinit.nix
+++ modules/services/dbus/dbus-dinit.nix
@@ -11,10 +11,12 @@ in
   config = lib.mkIf (config.system.init == "dinit" && cfg.enable) {
     dinit.services.dbus = {
       type = "process";
-      command = pkgs.writeShellScript "dbus-dinit-start" ''
-        ${cfg.package}/bin/dbus-uuidgen --ensure
-        exec ${cfg.package}/bin/dbus-daemon --nofork --system --syslog-only
-      '';
+      command = toString (
+        pkgs.writeShellScript "dbus-dinit-start" ''
+          ${cfg.package}/bin/dbus-uuidgen --ensure
+          exec ${cfg.package}/bin/dbus-daemon --nofork --system --syslog-only
+        ''
+      );
       waits-for =
         lib.optional (config.services ? sysklogd && config.services.sysklogd.enable) "syslogd"
         ++ [ "tmpfiles-setup" ];
