9558c2db9d75c66a296137bf236d969e5a6606b8
Paweł Sobczak <github@fixeq.qzz.io>
2026-08-01 15:23:53 +0200

    fix(boot/modprobe): make kmod in initrd unconditional

--- modules/boot/modprobe.nix
+++ modules/boot/modprobe.nix
@@ -18,36 +18,42 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    boot.initrd.path = [ pkgs.kmod ];
+  config = lib.mkMerge [
+    {
+      # unconditional: the initramfs needs kmod regardless of
+      # boot.modprobeConfig.enable (which only toggles the stage-2 modprobe setup).
+      boot.initrd.path = [ pkgs.kmod ];
+    }
 
-    environment.etc."modules-load.d/finix.conf" = {
-      text = ''
-        set modprobe = ${pkgs.kmod}/bin/modprobe
+    (lib.mkIf cfg.enable {
+      environment.etc."modules-load.d/finix.conf" = {
+        text = ''
+          set modprobe = ${pkgs.kmod}/bin/modprobe
 
-        ${lib.concatStringsSep "\n" config.boot.kernelModules}
+          ${lib.concatStringsSep "\n" config.boot.kernelModule3s}
+        '';
+      };
+
+      environment.etc."modprobe.d/ubuntu.conf".source = "${pkgs.kmod-blacklist-ubuntu}/modprobe.conf";
+      environment.etc."modprobe.d/debian.conf".source = pkgs.kmod-debian-aliases;
+
+      environment.systemPackages = [
+        pkgs.kmod
+      ];
+
+      finit.tasks.modprobe = {
+        command = "${pkgs.kmod}/bin/modprobe --all ${lib.concatStringsSep " " config.boot.kernelModules}";
+        conditions = "service/syslogd/ready";
+        runlevel = "12345789";
+      };
+
+      system.activation.scripts.modprobe = ''
+        # Allow the kernel to find our wrapped modprobe (which searches
+        # in the right location in the Nix store for kernel modules).
+        # We need this when the kernel (or some module) auto-loads a
+        # module.
+        echo ${pkgs.kmod}/bin/modprobe > /proc/sys/kernel/modprobe
       '';
-    };
-
-    environment.etc."modprobe.d/ubuntu.conf".source = "${pkgs.kmod-blacklist-ubuntu}/modprobe.conf";
-    environment.etc."modprobe.d/debian.conf".source = pkgs.kmod-debian-aliases;
-
-    environment.systemPackages = [
-      pkgs.kmod
-    ];
-
-    finit.tasks.modprobe = {
-      command = "${pkgs.kmod}/bin/modprobe --all ${lib.concatStringsSep " " config.boot.kernelModules}";
-      conditions = "service/syslogd/ready";
-      runlevel = "12345789";
-    };
-
-    system.activation.scripts.modprobe = ''
-      # Allow the kernel to find our wrapped modprobe (which searches
-      # in the right location in the Nix store for kernel modules).
-      # We need this when the kernel (or some module) auto-loads a
-      # module.
-      echo ${pkgs.kmod}/bin/modprobe > /proc/sys/kernel/modprobe
-    '';
-  };
+    })
+  ];
 }
