5c014f5c5de7272f0699e9cb030cc081b30c9945
willowispll <gorelikgleb+github@gmail.com>
2026-09-12 17:16:41 +0300

    Update default.nix

--- modules/programs/vxwm/default.nix
+++ modules/programs/vxwm/default.nix
@@ -8,22 +8,11 @@
 let
   cfg = config.programs.vxwm;
 
-  sessionScript = pkgs.writeTextFile {
-    name = "vxwm-session";
-    executable = true;
-    destination = "/bin/vxwm-session";
-    text = ''
-      #!${pkgs.dash}/bin/dash
-      ${lib.concatStringsSep "\n" cfg.autostart}
-      exec ${cfg.package}/bin/vxwm
-    '';
-  };
-
   sessionFile = pkgs.writeTextDir "share/xsessions/vxwm.desktop" ''
     [Desktop Entry]
     Name=vxwm
     Comment=Versatile X Window Manager
-    Exec=${pkgs.dbus}/bin/dbus-run-session -- ${sessionScript}/bin/vxwm-session
+    Exec=${pkgs.dbus}/bin/dbus-run-session -- ${cfg.package}/bin/vxwm
     TryExec=${cfg.package}/bin/vxwm
     Type=Application
     DesktopNames=vxwm
@@ -33,24 +22,13 @@ in
   imports = [ modules.xorg ];
 
   options.programs.vxwm = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Whether to enable [vxwm](${pkgs.vxwm.meta.homepage}).";
-    };
+    enable = lib.mkEnableOption "vxwm";
 
     package = lib.mkOption {
       type = lib.types.package;
       default = pkgs.vxwm;
       defaultText = lib.literalExpression "pkgs.vxwm";
       description = "The package to use for `vxwm`.";
-    };
-
-    autostart = lib.mkOption {
-      type = with lib.types; listOf str;
-      default = [ ];
-      example = lib.literalExpression ''[ "pipewire &" "picom &" ]'';
-      description = "Commands to run before starting vxwm. Each string is a shell command.";
     };
   };
 
